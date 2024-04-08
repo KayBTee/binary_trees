49f2eec4799ceda343ca3f561e5f37ef047920c5
root <kbthaba@gmail.com>
2024-04-08 02:56:46 -0700

    Write a function that goes through a binary tree using pre-order traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,17 @@
+nclude "binary_trees.h"
+/**
+ * binary_tree_preorder - Traverses a binary tree using pre_order traversal.
+ * @tree: A pointer to the root node of the ree to traverse.
+ * @func: A pointer to a function to call for each node's value.
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+	{
+		return;
+	}
+	func(tree->n);
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
